70233dbe2311c2c3bf1e817dec511de102e98d3c
Jzackiewicz <jj.zackiewicz@gmail.com>
2025-08-29 17:25:08 +0200

    .

--- include/rt/Config.hpp
+++ include/rt/Config.hpp
@@ -2,8 +2,8 @@
 
 // Gameplay configuration constants
 
-#define SCROLL_STEP 1.0
-#define OBJECT_MOVE_SPEED 15.0
+#define SCROLL_STEP 0.1
+#define OBJECT_MOVE_SPEED 10.0
 #define CAMERA_MOVE_SPEED 15.0
 #define MOUSE_SENSITIVITY 0.002
 
