c30655833a484831c789600c9703e3b1e4c9fa5b
Jzackiewicz <jj.zackiewicz@gmail.com>
2025-09-05 14:28:35 +0200

    Refactor beam system into composite class

---
+++ include/rt/Laser.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include "Hittable.hpp"
+#include "Ray.hpp"
+#include <memory>
+
+namespace rt
+{
+struct Laser : public Hittable
+{
+  Ray path;
+  double radius;
+  double length;
+  double start;
+  double total_length;
+  double light_intensity;
+  std::weak_ptr<Hittable> source;
+  Laser(const Vec3 &origin, const Vec3 &dir, double radius, double length,
+        double intensity, int oid, int mid, double start = 0.0,
+        double total = -1.0);
+
+  bool hit(const Ray &r, double tmin, double tmax, HitRecord &rec) const override;
+  bool bounding_box(AABB &out) const override;
+  bool is_beam() const override;
+  ShapeType shape_type() const override { return ShapeType::Beam; }
+  Vec3 spot_direction() const override { return path.dir; }
+};
+
+} // namespace rt
