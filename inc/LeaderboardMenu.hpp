5a6b43fdf5b4b7ad3bebf248d056c52167d11f1b
Jzackiewicz <jj.zackiewicz@gmail.com>
2025-09-12 23:29:24 +0200

    Use ordered map for leaderboard and integrate menu navigation

--- inc/LeaderboardMenu.hpp
+++ inc/LeaderboardMenu.hpp
@@ -1,12 +1,24 @@
 #pragma once
 #include "AMenu.hpp"
+#include <map>
+#include <functional>
 
 struct SDL_Window;
 struct SDL_Renderer;
 
 // Menu showing the leaderboard
 class LeaderboardMenu : public AMenu {
+private:
+    // Ordered map of score to player name, sorted by score descending
+    std::multimap<double, std::string, std::greater<double>> records;
+
+    // Load leaderboard records from a YAML file
+    void load_records(const std::string &path);
+
 public:
     LeaderboardMenu();
     static void show(SDL_Window *window, SDL_Renderer *renderer, int width, int height);
+
+    // Run the leaderboard menu loop
+    ButtonAction run(SDL_Window *window, SDL_Renderer *renderer, int width, int height);
 };
