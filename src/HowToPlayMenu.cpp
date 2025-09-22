e857c2d7994f4b4c4f6e28a1b0ae662c193dc532
Jzackiewicz <jj.zackiewicz@gmail.com>
2025-09-22 02:06:03 +0200

    Ensure How To Play title renders in white

--- src/HowToPlayMenu.cpp
+++ src/HowToPlayMenu.cpp
@@ -3,6 +3,7 @@
 #include <SDL.h>
 
 HowToPlayMenu::HowToPlayMenu() : AMenu("HOW TO PLAY") {
+    title_colors = {SDL_Color{255, 255, 255, 255}};
     buttons_align_bottom = true;
     buttons_bottom_margin = 60;
     buttons.push_back(Button{"BACK", ButtonAction::Back, SDL_Color{255, 0, 0, 255}});
