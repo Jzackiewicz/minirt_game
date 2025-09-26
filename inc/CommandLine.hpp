aa85ad996bc8022b1cbe9b46213ed761d714f338
Jzackiewicz <jj.zackiewicz@gmail.com>
2025-09-26 12:24:16 +0200

    Add optional scene path argument to skip main menu

--- inc/CommandLine.hpp
+++ inc/CommandLine.hpp
@@ -3,11 +3,12 @@
 #include <string>
 
 /**
- * Prepares the default scene path.
+ * Parses command line arguments and determines the starting scene.
  *
- * @param argc Argument count (unused).
- * @param argv Argument values (unused).
+ * @param argc Argument count.
+ * @param argv Argument values.
  * @param scene_path Output path to scene file.
+ * @param skip_main_menu Set to true when a scene is provided on the command line.
  * @return True on success.
  */
-bool parse_arguments(int argc, char **argv, std::string &scene_path);
+bool parse_arguments(int argc, char **argv, std::string &scene_path, bool &skip_main_menu);
